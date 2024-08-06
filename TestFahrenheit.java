32dc8afa10a41a45de28696973cbf577c0c800be
Sommai-Supawong <Sommai.050449@gmail.com>
2024-08-06 10:49:14 +0700

    Scanner

---
+++ TestFahrenheit.java
@@ -0,0 +1,12 @@
+
+
+public class TestFahrenheit {
+    public static void main (String[]args){
+     float Fahrenheit = 80.0f;
+     float celsius =(Fahrenheit - 32) * 5/9;
+     System.out.println( "celsius = " +celsius +"bye bye!" );
+
+
+    }
+    
+}
